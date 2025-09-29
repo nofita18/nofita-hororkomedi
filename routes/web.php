44f6ae835ef5860123d5d6d4e4b79ac6328f319f
nofita18 <nofita24si@mahasiswa.pcr.ac.id>
2025-09-29 10:11:22 +0700

    submit form pertanyaan

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\MahasiswaController;
 
 Route::get('/', function () {
@@ -35,3 +36,6 @@ Route::get('/about', function () {
 Route::get('/home',[HomeController::class,'index']);
 
 Route::get('/profile', [PegawaiController::class,'index']);
+
+Route::post('question/store', [QuestionController::class, 'store'])
+		->name('question.store');
